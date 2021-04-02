d1cc9f2b579cf092a9cf697cee61e06bdf798ed7
ubb-map <tirdeaandrei@gmail.com>
2021-04-02 21:11:12 +0300

    Deleted old project

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import org.hibernate.cfg.Configuration;
-
-public class Main {
-    public static void main(String[] args) {
-        Configuration con = new Configuration().configure("hibernate.cfg.xml");
-        System.out.println("Maven Project!");
-    }
-}
-//proposal & section
