a59cf726054cb80ab200989efb67b70f7c48e5de
Diana Tibre <diana.tibre@gmail.com>
2021-04-06 22:00:03 +0300

    Paper service

--- store-core/src/main/java/core/service/PaperService.java
+++ store-core/src/main/java/core/service/PaperService.java
@@ -2,6 +2,8 @@ package core.service;
 
 import core.domain.Evaluation;
 import core.domain.Paper;
+import core.domain.Reviewer;
+import core.domain.Section;
 import core.repository.EvaluationRepoI;
 import core.repository.PaperRepoI;
 import core.repository.ProposalRepoI;
@@ -25,9 +27,13 @@ public class PaperService {
     private ProposalRepoI proposalRepo;
 
     public boolean addPaper(Paper paper) {
-        Optional<Paper> pap = repo.findById(paper.getId());
-        if (pap.isPresent())
+        if(paper.getTitle().equals("") || paper.getDocument().equals("") || !proposalRepo.findById(paper.getProposal().getId()).isPresent()){
             return false;
+        }
+        Optional<Paper> pap = repo.findById(paper.getId());
+        if (pap.isPresent()) {
+            return false;
+        }
         repo.save(paper);
         return true;
     }
@@ -36,11 +42,42 @@ public class PaperService {
         return repo.findById(id);
     }
 
+    public boolean deletePaper(Integer id) {
+        Optional<Paper> paper = this.repo.findById(id);
+        if (paper.isPresent()) {
+            this.repo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Transactional
+    public boolean updatePaper(Paper paper) {
+        if(paper.getTitle().equals("") || paper.getDocument().equals("") || !proposalRepo.findById(paper.getProposal().getId()).isPresent()){
+            return false;
+        }
+        this.repo.findById(paper.getId()).ifPresent(s -> {
+            s.setTitle(paper.getTitle());
+            s.setDocument(paper.getDocument());
+            s.setProposal(paper.getProposal());
+            s.setSpeaker(paper.getSpeaker());
+            s.setEvaluations(paper.getEvaluations());
+        });
+        return true;
+    }
+
 
     public boolean addEvaluation(Evaluation evaluation) {
         //find the number of evaluations of the specific paper, and if it's greater than 4, deny the operation
         Optional<Evaluation> pro = evalRepo.findById(evaluation.getId());
         if (evalRepo.findEvaluationsByPaperId(pro.get().getPaper().getId()).size() >= 4) {
+            return false;
+        }
+        if (evaluation.getResult().equals(""))
+            return false;
+
+        Optional<Evaluation> ev = this.evalRepo.findById(evaluation.getId());
+        if (ev.isPresent()) {
             return false;
         }
 
@@ -66,10 +103,22 @@ public class PaperService {
     }
 
     public boolean updateEvaluationResult(Evaluation evaluation) {
+        if (evaluation.getResult().equals(""))
+            return false;
         Evaluation e = this.evalRepo.findById(evaluation.getId()).get();
         e.setResult(evaluation.getResult());
         this.evalRepo.save(e);
         return true;
+    }
+
+    public boolean deleteEvaluation(Integer id) {
+        Optional<Evaluation> evaluation = this.evalRepo.findById(id);
+
+        if (evaluation.isPresent()){
+            this.evalRepo.deleteById(id);
+            return true;
+        }
+        return false;
     }
 
     public List<Paper> getPapersByReviewer(Integer reviewerID) {
@@ -77,3 +126,4 @@ public class PaperService {
         return evaluations.stream().filter(e -> e.getReviewer().getId() == reviewerID).map(e -> e.getPaper()).collect(Collectors.toList());
     }
 }
+
