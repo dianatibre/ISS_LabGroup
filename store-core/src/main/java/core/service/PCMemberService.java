75c26a33e07477fec72c436cd0b6d3052ba85601
timitamas24 <30402814+timitamas24@users.noreply.github.com>
2021-04-06 21:42:13 +0300

    Added functions in PCMemberService

--- store-core/src/main/java/core/service/PCMemberService.java
+++ store-core/src/main/java/core/service/PCMemberService.java
@@ -33,7 +33,7 @@ public class PCMemberService {
 
 
     public boolean addPCMember(PCMember member) {
-       if(member.getName().equals("") || member.getConferences().isEmpty() || member.getLogin().isEmpty())
+       if(member.getName().equals("") || member.getAffiliation().equals("") || member.getEmail().equals("") || member.getWebsite().equals(""))
            return false;
 
        Optional<PCMember> mem = this.repo.findById(member.getId());
@@ -48,8 +48,33 @@ public class PCMemberService {
         return repo.findById(id);
     }
 
+    public boolean deletePCMember(Integer id) {
+        Optional<PCMember> mem = this.repo.findById(id);
+
+        if (mem.isPresent()){
+            this.repo.deleteById(id);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Transactional
+    public boolean updatePCMember(PCMember member){
+        if(member.getName().equals("") || member.getAffiliation().equals("") || member.getEmail().equals("") || member.getWebsite().equals(""))
+            return false;
+
+        this.repo.findById(member.getId()).ifPresent(m -> {
+            m.setName(member.getName());
+            m.setAffiliation(member.getAffiliation());
+            m.setEmail(member.getEmail());
+            m.setWebsite(member.getWebsite());
+        });
+        return true;
+    }
+
     public boolean addReviewer(Reviewer reviewer) {
-        if(reviewer.getName().equals("") || reviewer.getBiddings().isEmpty() )
+        if(reviewer.getDomainOfInterest().equals(""))
             return false;
 
         Optional<Reviewer> re = this.reviewerRepo.findById(reviewer.getId());
@@ -68,7 +93,36 @@ public class PCMemberService {
             return Optional.empty();
     }
 
+    public Optional<Reviewer> findOneReviewer(Integer id) {
+        return reviewerRepo.findById(id);
+    }
+
+    public boolean deleteReviewer(Integer id) {
+        Optional<Reviewer> rew = this.reviewerRepo.findById(id);
+
+        if (rew.isPresent()){
+            this.reviewerRepo.deleteById(id);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Transactional
+    public boolean updateReviewer(Reviewer reviewer){
+        if(reviewer.getDomainOfInterest().equals(""))
+            return false;
+
+        this.reviewerRepo.findById(reviewer.getId()).ifPresent(r -> {
+           r.setDomainOfInterest(reviewer.getDomainOfInterest());
+        });
+        return true;
+    }
+
     public boolean addChair(Chair chair) {
+        if(chair.getPosition().equals(""))
+            return false;
+
         Optional<Chair> p = chairRepo.findById(chair.getId());
         if (p.isPresent())
             return false;
@@ -83,14 +137,40 @@ public class PCMemberService {
         else
             return Optional.empty();
     }
-/*
-    public Optional<Chair> getChairBySectionID(int sectionId) {
+
+    /*public Optional<Chair> getChairBySectionID(int sectionId) {
         Optional<Integer> chairId = sectionRepo.findAll().stream().filter(s -> s.getId() == sectionId).map(s-> s.getSectionChair().getId()).findAny();
         if (chairId.isPresent()) {
             return chairRepo.findById(chairId.get());
         } else
             return Optional.empty();
     }*/
+
+    public boolean deleteChair(Integer id) {
+        Optional<Chair> cha = this.chairRepo.findById(id);
+
+        if (cha.isPresent()){
+            this.reviewerRepo.deleteById(id);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Transactional
+    public boolean updateChair(Chair chair){
+        if(chair.getPosition().equals(""))
+            return false;
+
+        this.chairRepo.findById(chair.getId()).ifPresent(c -> {
+            c.setPosition(chair.getPosition());
+        });
+        return true;
+    }
+
+    public Optional<Chair> findOneChair(Integer id) {
+        return chairRepo.findById(id);
+    }
 
     public Optional<PCMember> getPCMemberByUsername(String username) {
         Optional<Login> login = loginRepo.findAll().stream().filter(l -> l.getUsername().equals(username)).findAny();
@@ -109,22 +189,34 @@ public class PCMemberService {
         return reviewerRepo.findAll();
     }
 
+    public List<Chair> getAllChairs(){
+        return chairRepo.findAll();
+    }
+
     @Transactional
     public boolean addSectionToListener(Integer listenerID, Section s) {
-        Listener listener = listenerRepo.findById(listenerID).get();
-       // List<Section> sections = listener.getSections();
-        Section se = sectionRepo.findById(s.getId()).get();
-       // sections.add(se);
-        listener.setSectionId(se);
-        this.listenerRepo.save(listener);
-        return true;
+        if(listenerRepo.findById(listenerID).isPresent()){
+            Listener listener = listenerRepo.findById(listenerID).get();
+            // List<Section> sections = listener.getSections();
+            Section se = sectionRepo.findById(s.getId()).get();
+            // sections.add(se);
+            listener.setSectionId(se);
+            this.listenerRepo.save(listener);
+            return true;
+        }
+        return false;
+
     }
 
     @Transactional
     public long numberListenersPerSection(Integer sectionID) {
-        Section s = sectionRepo.findById(sectionID).get();
-        List<Listener> listeners = listenerRepo.findAll();
-        return listeners.stream().filter(l -> l.getSectionId().equals(s)).count();
+        if(sectionRepo.findById(sectionID).isPresent()){
+            Section s = sectionRepo.findById(sectionID).get();
+            List<Listener> listeners = listenerRepo.findAll();
+            return listeners.stream().filter(l -> l.getSectionId().equals(s)).count();
+        }
+
+        return (long)(0);
     }
 }
 
